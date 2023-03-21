195409868028a0f291a37fb4381c6a357806b2d2
ANDRÉ <127751657+andrehenrique20011@users.noreply.github.com>
2023-03-21 09:52:02 -0300

    MULTIPLO DE ALGUM NUMERO

---
+++ aulatesteees.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class aulatesteees {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	
+	System.out.println("Informe um número :");
+	int n1 = sc.nextInt();
+	
+	
+	if(n1 % 3 ==0) {
+		 System.out.println("É multiplo de 3!!!");
+		 
+	}else {
+	  System.out.println("Não múltiplo de 3!!!");	
+	
+	}
+		// TODO Auto-generated method stub
+	
+  
+	}
+
+}
